77f2e2bf8de3501b7c1f63c85533934e8b86a3ec
Nerpp <regiaurelien@gmail.com>
2021-09-03 11:12:52 +0200

    restrain /user to admin

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/", name="default")
+     * @Route("/homepage", name="default")
      */
     public function index(): Response
     {
