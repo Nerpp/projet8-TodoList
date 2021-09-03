eed2c4d005777bfabba6aab5c9cae00ae846ff47
Nerpp <regiaurelien@gmail.com>
2021-09-03 14:28:53 +0200

    configuration logout

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/homepage", name="default")
+     * @Route("/", name="default")
      */
     public function index(): Response
     {
