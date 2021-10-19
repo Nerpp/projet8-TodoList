ccb6df1c44498ca9465e2f793e28ee7d4e311d81
Nerpp <regiaurelien@gmail.com>
2021-10-19 08:31:57 +0200

    correction security controller

--- tests/Controller/SecurityControllerTest.php
+++ tests/Controller/SecurityControllerTest.php
@@ -10,11 +10,30 @@ use App\Repository\UserRepository;
 
 class SecurityControllerTest extends WebTestCase
 {
+
+    private function client()
+    {
+        return static::createClient();
+    }
+
+    private function user($client)
+    {
+        // $client = static::createClient();
+        $userRepository = static::getContainer()->get(UserRepository::class);
+
+        // retrieve the test user
+        $grabUser = $userRepository->findOneByEmail('francis@gmail.com');
+
+        // simulate $testUser being logged in
+        return $client->loginUser($grabUser);   
+    }
     
     public function testLogginIsUp(): void
     {
-        $client = static::createClient();
+    
+        $client = $this->client();
         $client->request('GET', '/login');
+        
         $this->assertResponseIsSuccessful();
         
         echo $client->getResponse()->getContent();
@@ -22,33 +41,35 @@ class SecurityControllerTest extends WebTestCase
 
     public function testLogginPage()
     {
-        $client = static::createClient();
-        $crawler = $client->request('GET', '/login');
+        
+        $crawler = $this->client()->request('GET', '/login');
         $this->assertSelectorTextContains('h1', 'Connection');
 
     }
 
     public function testLoggin()
     {
-        $client = static::createClient();
+       
+        $client = $this->client();
+
         $crawler = $client->request('GET', '/login');
 
-        // $form = $crawler->selectButton('Se connecter')->form([
-        //     'username' => 'francis@gmail.com',
-        //     'password' => '12345678'
-        // ]);
-
+        
         // select the button
         $buttonCrawlerNode = $crawler->selectButton('Se connecter');
 
-        // retrieve the Form object for the form belonging to this button
         $form = $buttonCrawlerNode->form();
-
         // set values on a form object
         $form['username'] = 'francis@gmail.com';
         $form['password'] = '12345678';
         
         $crawler = $client->submit($form);
+
+        // autre alternative
+        // $form = $crawler->selectButton('Se connecter')->form([
+        //     'username' => 'francis@gmail.com',
+        //     'password' => '12345678'
+        // ]);
 
         if ($client->getResponse()->isRedirection()) {
            $crawler = $client->followRedirect();
