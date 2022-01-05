19982e078e03fce1422837150fa4d7af47b3505e
Nerpp <regiaurelien@gmail.com>
2022-01-05 19:29:38 +0100

    composer update && yarn upgrade

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,8 +6,7 @@ use Doctrine\ORM\Mapping as ORM;
 use App\Repository\UserRepository;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\Common\Collections\ArrayCollection;
-use Symfony\Component\Validator\Constraints\Regex;
-use Symfony\Component\Validator\Constraints\NotBlank;
+
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
